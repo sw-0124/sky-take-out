949e8cd027f9bb120e98e012aa315a4560cebee5
tomato <2535981850@qq.com>
2024-08-13 11:00:01 +0800

    商品浏览业务功能实现

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public interface DishService {
 
+
     /**
      * 新增菜品和口味
      * @param dishDTO
@@ -59,4 +60,12 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categoryId, String name);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
+
 }
